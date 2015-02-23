a71267a75a7269a36965e8f55f583d4f34e015bf
George Vorobyev <quaffle97@gmail.com>
2015-02-23 10:33:32 -0500

    tweaked location shenanigans

--- camera.cpp
+++ camera.cpp
@@ -14,23 +14,23 @@ void Camera::renderMesh(Mesh *m, Surface *s) {
 		Vect4 v2 = m->verts[m->faces[i].v2 - 1];
 		Vect4 v3 = m->verts[m->faces[i].v3 - 1];
 
-		v1.x *= 600;
-		v1.x += 400;
+		v1.x *= 400;
+		v1.x += 500;
 
-		v1.y *= 600;
-		v1.y += 400;
+		v1.y *= 500;
+		v1.y += 300;
 
-		v2.x *= 600;
-		v2.x += 400;
+		v2.x *= 400;
+		v2.x += 500;
 
-		v2.y *= 600;
-		v2.y += 400;
+		v2.y *= 500;
+		v2.y += 300;
 
-		v3.x *= 600;
-		v3.x += 400;
+		v3.x *= 400;
+		v3.x += 500;
 
-		v3.y *= 600;
-		v3.y += 400;
+		v3.y *= 500;
+		v3.y += 300;
 
 		g.drawLine((int)v1.x, (int)v1.y, (int)v2.x, (int)v2.y);
 		g.drawLine((int)v2.x, (int)v2.y, (int)v3.x, (int)v3.y);
